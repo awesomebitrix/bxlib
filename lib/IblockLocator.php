7b9923348fa1d1d69a0529e41832cd13d2f4e985
marv <marv@marv.marv>
2015-12-17 21:35:26 +0500

    add iblock locator

---
+++ lib/IblockLocator.php
@@ -0,0 +1,140 @@
+<?php
+
+namespace marvin255\bxlib;
+
+class IblockLocator
+{
+	/**
+	 * @var array
+	 */
+	public $filter = [
+		'ACTIVE' => 'Y',
+		'CHECK_PERMISSIONS' => 'N',
+	];
+	/**
+	 * @var array
+	 */
+	public $select = [
+		'ID',
+		'CODE',
+	];
+	/**
+	 * @var int
+	 */
+	public $cacheTime = 2592000;
+	/**
+	 * @var array
+	 */
+	protected $_list = null;
+	/**
+	 * @var mixed
+	 */
+	protected $_cache = null;
+
+
+	public function __construct()
+	{
+		\CModule::IncludeModule('iblock');
+	}
+
+
+	/**
+	 * @param string $code
+	 * @return string
+	 */
+	public function getIdByCode($code)
+	{
+		return $this->findBy('CODE', $code, 'ID');
+	}
+
+
+	/**
+	 * @param string $field
+	 * @param mixed $value
+	 * @param string $select
+	 * @return array
+	 */
+	public function findBy($field, $value, $select = null)
+	{
+		$return = null;
+		$list = $this->getList();
+		foreach ($list as $iblock) {
+			if (isset($iblock[$field]) && $iblock[$field] == $value) {
+				if ($select) {
+					$return = isset($iblock[$select]) ? $iblock[$select] : null;
+				} else {
+					$return = $iblock;
+				}
+				break;
+			}
+
+		}
+		return $return;
+	}
+
+	/**
+	 * @param string $field
+	 * @param mixed $value
+	 * @param string $select
+	 * @return array
+	 */
+	public function findAllBy($field, $value, $select = null)
+	{
+		$return = null;
+		$list = $this->getList();
+		foreach ($list as $iblock) {
+			if (isset($iblock[$field]) && $iblock[$field] != $value) {
+				if ($select) {
+					$return[] = isset($iblock[$select]) ? $iblock[$select] : null;
+				} else {
+					$return[] = $iblock;
+				}
+			}
+
+		}
+		return $return;
+	}
+
+	/**
+	 * @return array
+	 */
+	protected function getList()
+	{
+		if ($this->_list === null) {
+			$cache = $this->getCache();
+			$cId = get_class($this);
+			if (!$cache || ($this->_list = $cache->get($cId)) === false) {
+				$this->_list = [];
+				$res = \CIblock::GetList([], $this->filter);
+				while ($ob = $res->GetNext()) {
+					$arItem = [];
+					foreach ($this->select as $field) {
+						if (!isset($ob[$field])) continue;
+						$arItem[$field] = $ob[$field];
+					}
+					$this->_list[] = $arItem;
+				}
+				if ($cache) $cache->set($cId, $this->_list, $this->cacheTime);
+			}
+		}
+		return $this->_list;
+	}
+
+
+	/**
+	 * @param \marvin255\bxcache\ICache $cache
+	 */
+	public function setCache(\marvin255\bxcache\ICache $cache)
+	{
+		$this->_cache = $cache;
+		return $this;
+	}
+
+	/**
+	 * @return \marvin255\bxcache\ICache
+	 */
+	public function getCache()
+	{
+		return $this->_cache;
+	}
+}
